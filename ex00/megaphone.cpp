ac89c55dec84788e2455e7fc62c25d0f4f2d647e
kinamura <i.kosuke0801@gmail.com>
2025-10-19 20:18:47 +0900

    fix: type c++

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,36 +1,16 @@
 #include <iostream>
 
-void	toUpper(char **str)
-{
-	int	i;
-
-	i = 0;
-	while ((*str)[i])
-	{
-		if ((*str)[i] >= 'a' && (*str)[i] <= 'z')
-			(*str)[i] -= 32;
-		i++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	int	i;
-
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
-	else
-	{
-		i = 1;
-		while (argv[i])
-		{
-			toUpper(&argv[i]);
-			std::cout << argv[i];
-			i++;
-		}
-		std::cout << std::endl;
-	}
-	return (0);
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    for (int i = 1; argv[i]; ++i) {
+        for (int j = 0; argv[i][j]; ++j) {
+            char upper = std::toupper(static_cast<unsigned char>(argv[i][j]));
+            std::cout << upper;
+        }
+    }
+    std::cout << std::endl;
+    return 0;
 }
